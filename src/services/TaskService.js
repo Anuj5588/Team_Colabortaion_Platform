61e890ac7e63bd6e76dc0e30f70991ff22f6cd1e
Anuj5588 <ak4826636@gmail.com>
2026-03-31 22:17:55 +0530

    added some services for the task

--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -1,17 +1,39 @@
 import Task from "../models/Task.js";
 
 export const   createTask= async (data)=>{
-    console.log(data)
-
     const task = await Task.create(data)
-
     return task
-
-
 }
 
 export const  getTask = async (data)=>{
-
     const task = await Task.find({data})
     return task
 }
+
+export const  getSingleTask = async (data)=>{
+    const task = await Task.findById({data})
+    return task
+}
+
+export const  UpdateTask = async (data)=>{
+    const task = await Task.findByIdAndUpdate({data})
+    return task
+}
+
+export const  deleteTask = async (data)=>{
+    const task = await Task.findOneAndDelete({data})
+    return task
+}
+ 
+export const  updateTaskStatus = async (data)=>{
+    const task = await Task.findOneAndUpdate({data})
+    return task
+}
+
+export const  assignTask = async (data)=>{
+    const task = await Task({data})
+    return task
+}
+
+
+
